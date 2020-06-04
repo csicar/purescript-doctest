6ebc75817371b91430de51e891c31a8e5a1e2d76
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2020-06-04 23:42:42 +0200

    Update test

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,13 +5,14 @@ import Purepur.Parser
 import Data.Text as T
 import qualified Language.PureScript.Names as Purs
 import qualified Language.PureScript.AST as AST
+import Purepur.Types
 import qualified Language.PureScript.Interactive.Types as Psci 
 
 
 main :: IO ()
 main = hspec $
   describe "Parse Comment" $ do
-    let importT = Command [ Psci.Import (Purs.ModuleName [Purs.ProperName "T"], AST.Implicit, Nothing) ]
+    let importT = Command $ Psci.Import (Purs.ModuleName [Purs.ProperName "T"], AST.Implicit, Nothing)
 
     it "simple output" $ 
       parseInfoBlock "asd" `shouldBe` Right [ExpectedOutput "asd"]
@@ -24,3 +25,6 @@ main = hspec $
 
     it "multi-statement" $
       parseInfoBlock "> import\n   T\n123" `shouldBe` Right [importT, ExpectedOutput "123"]
+  -- describe "Pretty Print" $ do
+  --   it "print qualified op" $
+  --     pretty
