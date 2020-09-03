bb91cc22a6141c14d1a092e14cd6527072479c83
Carsten Csiky (csicar) <carsten.csiky@gmail.com>
2020-09-03 23:52:46 +0200

    npm package

---
+++ npm/install.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// The following implementation comes from
+// https://github.com/justinwoo/npm-psc-package-bin-simple/blob/4d4efa6a4e2008c8a0a71f0b189c14c31b88e47b/install.js
+
+const request = require("request");
+const tar = require("tar");
+const version = "v0.1.1";
+const platform = { win32: "windows", darwin: "osx" }[process.platform] || "linux";
+const url = `https://github.com/csicar/purescript-doctest/releases/download/${version}/${platform}.tar.gz`
+console.log(url)
+request.get(url).pipe(tar.x({C: './'}));
